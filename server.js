03334c9c832f82fa40553521a293932a647c4225
venkatram64 <venkat.veerareddy@hotmail.com>
2017-11-18 08:30:44 +0530

    added code

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-const R = require('ramda');
+'use strict'
+
+//const R = require('ramda');
+import R from 'ramda';
 
 const from10 = R.range(10);
 const isEven = n => n % 2 === 0;
@@ -10,3 +13,45 @@ const avg = nums => sum(nums) / length(nums);
 const avgEvenSquenceFrom10 = R.pipe(from10, R.filter(isEven), R.map(square), avg);
 const result = avgEvenSquenceFrom10(21);
 console.log("result ", result);
+
+//currying
+const add = x => y => x + y;
+//or
+/*function add(x) {
+    return function(y){
+        return x + y;
+    }
+}*/
+
+const factorial = (n) => {
+    if(n < 2){
+        return 1;
+    }
+    return n * factorial(n - 1);
+};
+/*
+    1 stack frame = 48B
+    Max stack usage = 1MB
+*/
+/*
+    (function a(x){
+        if(!x){
+            return;
+        }
+        a(--x);
+    })(10);
+
+*/
+//Tail call optimization
+
+const optimizedFactorial = (n, accum = 1) =>{
+    if(n < 2) {
+        return accum;
+    }
+    return optimizedFactorial(n - 1, n * accum);
+};
+
+let result1 = optimizedFactorial(100);
+
+console.log("big factorial is ", result1);
+
